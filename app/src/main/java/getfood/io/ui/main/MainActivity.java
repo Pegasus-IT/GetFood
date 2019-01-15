ee76771fe59cf63e7f0a6ce077ef21fa0e0d50e6
Wesley van der Kop <mail@wesleyvanderkop.nl>
2019-01-15 20:49:43 +0100

    Working on navigation

---
+++ app/src/main/java/getfood/io/ui/main/MainActivity.java
@@ -0,0 +1,45 @@
+package getfood.io.ui.main;
+
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import getfood.io.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
+    BottomAppBar bottomAppBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.fab);
+        bottomAppBar = findViewById(R.id.bottom_navigation_view);
+
+        // Main line for setting menu in bottom navigation menu
+//        setSupportActionBar(bottomAppBar);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Action button is clicked", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", null)
+                        .show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+        return true;
+    }
+}
