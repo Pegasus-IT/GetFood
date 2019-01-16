df41221c5625aad3c697e4ac4f8bf0f779ffe4e0
Bart van Vliet <bart.van.vliet@hotmail.com>
2019-01-16 18:46:41 +0100

    Added adapter & general improvements
    - added list view adapters
    - added temp ShoppingList model
    - added default card design
    - improved manifest activities
    - updated strings & colours

--- app/src/main/java/getfood/io/ui/home/HomeActivity.java
+++ app/src/main/java/getfood/io/ui/home/HomeActivity.java
@@ -1,6 +1,7 @@
 package getfood.io.ui.home;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 import getfood.io.R;
+import getfood.io.adapters.HomeListAdapter;
+import getfood.io.models.ShoppingList;
 import getfood.io.ui.createlist.CreateListActivity;
 import getfood.io.ui.shoppinglist.ShoppingListActivity;
 
@@ -18,6 +26,9 @@ public class HomeActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private FloatingActionButton createListButton;
 
+    private ListView listView;
+    private HomeListAdapter homeListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +36,7 @@ public class HomeActivity extends AppCompatActivity {
 
         createListButton = findViewById(R.id.home_create_list_fab);
         toolbar = findViewById(R.id.toolbar);
+        listView = findViewById(R.id.home_listview);
 
         toolbar.setTitle(R.string.home_title);
         toolbar.setNavigationIcon(R.drawable.ic_menu_34);
@@ -34,6 +46,31 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent = new Intent(HomeActivity.this, CreateListActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+
+        View topPadding = new View(this);
+        topPadding.setMinimumHeight(20);
+
+
+        //TODO: Replace with real data
+        ArrayList<ShoppingList> shoppingList = new ArrayList<>();
+        shoppingList.add(new ShoppingList("Feest", "15 Jan", Color.parseColor("#427CFB")));
+        shoppingList.add(new ShoppingList("Week lijst", "15 Jan", Color.parseColor("#00D157")));
+        shoppingList.add(new ShoppingList("Weekend", "15 Jan", Color.parseColor("#FFBB00")));
+        shoppingList.add(new ShoppingList("Feest", "15 Jan", Color.parseColor("#427CFB")));
+        shoppingList.add(new ShoppingList("Week lijst", "15 Jan", Color.parseColor("#00D157")));
+        shoppingList.add(new ShoppingList("Weekend", "15 Jan", Color.parseColor("#FFBB00")));
+
+
+        homeListAdapter = new HomeListAdapter(this, shoppingList);
+        listView.setAdapter(homeListAdapter);
+        listView.addHeaderView(topPadding);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ShoppingList selectedShoppingList = (ShoppingList) adapterView.getItemAtPosition(i);
+                openShoppingListItem(selectedShoppingList);
             }
         });
 
@@ -56,5 +93,12 @@ public class HomeActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    private void openShoppingListItem(ShoppingList item) {
+        Intent intent = new Intent(HomeActivity.this, ShoppingListActivity.class);
+        intent.putExtra("selectedShoppingListItem", (Serializable) item);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 }
