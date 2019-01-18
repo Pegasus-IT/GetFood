4ee46310afeb3adb2cfd00db0bda97e72e3585ea
Wesley van der Kop <mail@wesleyvanderkop.nl>
2019-01-18 23:34:10 +0100

    Created Sign up

--- app/src/main/java/getfood/io/ui/sign_up/SignUpActivity.java
+++ app/src/main/java/getfood/io/ui/sign_up/SignUpActivity.java
@@ -2,20 +2,50 @@ package getfood.io.ui.sign_up;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import getfood.io.R;
+import getfood.io.data.local.Globals;
 import getfood.io.data.network.ApiException;
+import getfood.io.data.network.api.UserControllerApi;
+import getfood.io.models.SwaggerApiError;
+import getfood.io.models.User;
 import getfood.io.models.UserCreateModel;
 import getfood.io.ui.BaseActivity;
 import getfood.io.ui.login.LoginActivity;
+import getfood.io.util.PreferenceHelper;
 
 public class SignUpActivity extends BaseActivity {
 
     private EditText emailInput, passwordInput, firstnameInput, lastnameInput;
     private Button registerButton;
+
+    private UserControllerApi api;
+
+    private TextWatcher requiredTextWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start,
+                                      int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+            registerButton.setEnabled(emailInput.getText().length() != 0 &&
+                    passwordInput.getText().length() != 0 &&
+                    firstnameInput.getText().length() != 0 &&
+                    lastnameInput.getText().length() != 0);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +60,19 @@ public class SignUpActivity extends BaseActivity {
         // Buttons
         registerButton = findViewById(R.id.register);
 
-        registerButton.setOnClickListener((View v) -> {
-            try {
-                register(
-                        emailInput.getText().toString(),
-                        passwordInput.getText().toString(),
-                        firstnameInput.getText().toString(),
-                        lastnameInput.getText().toString());
-            } catch (ApiException e) {
-                e.printStackTrace();
-            }
-        });
+        emailInput.addTextChangedListener(requiredTextWatcher);
+        passwordInput.addTextChangedListener(requiredTextWatcher);
+        firstnameInput.addTextChangedListener(requiredTextWatcher);
+        lastnameInput.addTextChangedListener(requiredTextWatcher);
+
+        api = new UserControllerApi();
+
+        registerButton.setOnClickListener((View v) -> register(
+                emailInput.getText().toString(),
+                passwordInput.getText().toString(),
+                firstnameInput.getText().toString(),
+                lastnameInput.getText().toString()));
+        registerButton.setEnabled(false);
 
         View toLoginButton = findViewById(R.id.to_login);
         toLoginButton.setOnClickListener(v -> {
@@ -76,11 +108,39 @@ public class SignUpActivity extends BaseActivity {
         SignUpActivity.this.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 
-    private void register(String email, String password, String firstName, String lastName) throws ApiException {
+    private void register(String email, String password, String firstName, String lastName) {
         UserCreateModel request = new UserCreateModel();
         request.setEmail(email);
         request.setPassword(password);
         request.setFirstName(firstName);
         request.setLastName(lastName);
+
+        new Thread(() -> {
+            try {
+                this.onRegister(api.userControllerRegister(request));
+            } catch (ApiException err) {
+                this.onError(err);
+            }
+        }).start();
+    }
+
+    private void showSnackbar(String text, int color) {
+        View parentLayout = findViewById(android.R.id.content);
+        Snackbar.make(parentLayout, text, Snackbar.LENGTH_LONG)
+                .setAction("CLOSE", view -> {
+
+                })
+                .setActionTextColor(getResources().getColor(color))
+                .show();
+    }
+
+    private void onError(ApiException err) {
+        SwaggerApiError swaggerApiError = SwaggerApiError.parse(err.getResponseBody());
+        this.showSnackbar(swaggerApiError.getMessage(), android.R.color.holo_red_dark);
+    }
+
+    private void onRegister(User userControllerAuthenticate) {
+        System.out.println("Account Created!");
+        this.showSnackbar("Account Created!", android.R.color.holo_green_light);
     }
 }
