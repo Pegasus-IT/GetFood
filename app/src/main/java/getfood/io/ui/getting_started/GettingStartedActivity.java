c5b57b0e3433f85e04dae51ed71159765438e9fa
Wesley van der Kop <mail@wesleyvanderkop.nl>
2019-01-18 21:30:48 +0100

    Added Getting Started Page.

--- app/src/main/java/getfood/io/ui/getting_started/GettingStartedActivity.java
+++ app/src/main/java/getfood/io/ui/getting_started/GettingStartedActivity.java
@@ -1,30 +1,39 @@
-package getfood.io.ui.family;
+package getfood.io.ui.getting_started;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import getfood.io.R;
 import getfood.io.ui.BaseActivity;
+import getfood.io.ui.login.LoginActivity;
 
-public class FamilyActivity extends BaseActivity {
+public class GettingStartedActivity extends BaseActivity {
+
+    private TextView bottomText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        bottomText = findViewById(R.id.not_ready);
+
+        bottomText.setOnClickListener(view -> openAcitivity(new Intent(this, LoginActivity.class), true));
     }
 
     @Override
     protected int getLayoutResourceId() {
-        return R.layout.activity_getting_started;
+        return R.layout.activity_login;
     }
 
     @Override
     protected int getToolbarTitle() {
-        return R.string.family;
+        return 0;
     }
 
     @Override
     protected int getToolbarNavigationIcon() {
-        return R.drawable.ic_menu_34;
+        return 0;
     }
 
     @Override
