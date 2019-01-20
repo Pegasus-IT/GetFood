0c025ed99823611f1c0c72917ef858ec81c1e6e8
Wesley van der Kop <mail@wesleyvanderkop.nl>
2019-01-20 16:44:33 +0100

    Models documentation.

--- app/src/main/java/io/getfood/models/BasePresenter.java
+++ app/src/main/java/io/getfood/models/BasePresenter.java
@@ -3,6 +3,9 @@ package io.getfood.models;
 
 public interface BasePresenter {
 
+    /**
+     * This is the BasePresenter and with MVP every Presenter has a start functionality
+     */
     void start();
 
 }
--- app/src/main/java/io/getfood/models/SwaggerApiError.java
+++ app/src/main/java/io/getfood/models/SwaggerApiError.java
@@ -3,30 +3,63 @@ package io.getfood.models;
 import com.google.gson.Gson;
 
 public class SwaggerApiError {
+
+    /**
+     * Check if there are is a error
+     */
     private boolean error;
+
+    /**
+     * Error message
+     */
     private String message;
 
+    /**
+     * Swaggers api error
+     * @param error check if there is a error
+     * @param message error message
+     */
     public SwaggerApiError(boolean error, String message) {
         this.error = error;
         this.message = message;
     }
 
+    /**
+     * Check if there is an error
+     * @return error
+     */
     public boolean isError() {
         return error;
     }
 
+    /**
+     * Sets error check
+     * @param error check if there is a error
+     */
     public void setError(boolean error) {
         this.error = error;
     }
 
+    /**
+     * Get error message
+     * @return error message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets error message
+     * @param message error message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Creates a string of the given error
+     * @return error string
+     */
     @Override
     public String toString() {
         return "SwaggerApiError{" +
@@ -35,6 +68,11 @@ public class SwaggerApiError {
                 '}';
     }
 
+    /**
+     * Parses json error string
+     * @param json error string
+     * @return parsed swagger error
+     */
     public static SwaggerApiError parse(String json) {
         return new Gson().fromJson(json, SwaggerApiError.class);
     }
