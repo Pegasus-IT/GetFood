bbc576c57c4b2ce99b2fea5d1688837d8ba549fb
Wesley van der Kop <mail@wesleyvanderkop.nl>
2019-01-20 17:18:08 +0100

    Documented every activity

--- app/src/main/java/io/getfood/modules/auth/login/LoginContract.java
+++ app/src/main/java/io/getfood/modules/auth/login/LoginContract.java
@@ -1,9 +1,5 @@
 package io.getfood.modules.auth.login;
 
-import android.content.SharedPreferences;
-import android.text.Editable;
-
-import io.getfood.data.swagger.ApiException;
 import io.getfood.data.swagger.models.User;
 import io.getfood.models.BasePresenter;
 import io.getfood.models.BaseView;
@@ -12,21 +8,31 @@ public interface LoginContract {
 
     interface View extends BaseView<Presenter> {
 
+        /**
+         * Navigate to getting started activity
+         * @param user model
+         */
         void onLogin(User user);
+
         void onTokenValidated(User user);
 
         void setUsernameText(String text);
 
         void showFingerAuth();
+
         void showFingerPrintButton();
+
         void hideFingerPrintButton();
+
         void setLoginButtonEnabled(boolean state);
+
         void setFingerprintButtonEnabled(boolean state);
     }
 
     interface Presenter extends BasePresenter {
 
         void login(String username, String password);
+
         void checkValid(String username, String password);
 
         void validateStoredToken();
